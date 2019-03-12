5675da1cd3435014c1e91a0ee35ed7274a007c6b
35385 <35385@DESKTOP-GA0VFQ8>
2019-03-12 17:14:40 +0000

    Changing PPS validation

--- src/EmployeeDetails.java
+++ src/EmployeeDetails.java
@@ -653,14 +653,13 @@ public class EmployeeDetails extends JFrame implements ActionListener, ItemListe
 
 	// check for correct PPS format and look if PPS already in use
 	public boolean correctPps(String pps, long currentByte) {
-		boolean ppsExist = false;
+		boolean ppsExist = true;
 		// check for correct PPS format based on assignment description
-		if (pps.length() == 8 || pps.length() == 9) {
+		if (pps.length() == 7) {
 			if (Character.isDigit(pps.charAt(0)) && Character.isDigit(pps.charAt(1))
 					&& Character.isDigit(pps.charAt(2))	&& Character.isDigit(pps.charAt(3)) 
-					&& Character.isDigit(pps.charAt(4))	&& Character.isDigit(pps.charAt(5)) 
-					&& Character.isDigit(pps.charAt(6))	&& Character.isLetter(pps.charAt(7))
-					&& (pps.length() == 8 || Character.isLetter(pps.charAt(8)))) {
+					&& Character.isDigit(pps.charAt(4)) 
+					&& Character.isDigit(pps.charAt(5))	&& Character.isLetter(pps.charAt(6))) {
 				// open file for reading
 				application.openReadFile(file.getAbsolutePath());
 				// look in file is PPS already in use
