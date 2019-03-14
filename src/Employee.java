ee329497c36891766d76b474886640ea0e17edcf
35385 <35385@DESKTOP-GA0VFQ8>
2019-03-14 19:00:38 +0000

    toString() changed as the if statement was redundant, this making the code more concise and get rid of a bad smell.

--- src/Employee.java
+++ src/Employee.java
@@ -107,14 +107,10 @@ public class Employee{
 
 	// Display Employee details
 	public String toString() {
-		String bool = "";
-		if (fullTime)
-			bool = "Yes";
-		else
-			bool = "No";
+		
 
 		return "Employee ID: " + this.employeeId + "\nPPS Number: " + this.pps + "\nSurname: " + this.surname
 				+ "\nFirst Name: " + this.firstName + "\nGender: " + this.gender + "\nDepartment: " + this.department + "\nSalary: " + this.salary
-				+ "\nFull Time: " + bool;
+				+ "\nFull Time: " + this.fullTime;
 	}// end toString
 }// end class Employee
